5e12fdaf5c4af39e1d8b343b441ebab61abfe2da
Joseph Ashby <jashby473@gmail.com>
2016-02-18 14:49:03 -0800

    forced to commit

--- client/app/application.js
+++ client/app/application.js
@@ -3,19 +3,19 @@ angular.module('forinlanguages', [
   'LocalForageModule',
   'forinlanguages.services',
   'forinlanguages.peer'
-])
-<<<<<<< a5d4e2568a05019467fa07cc6e93964689c617d1
-.config(['$localForageProvider', function($localForageProvider){
-  $localForageProvider.setNotify(true, true); // itemSet, itemRemove
-}]);
-=======
+]);
+// <<<<<<< a5d4e2568a05019467fa07cc6e93964689c617d1
+// .config(['$localForageProvider', function($localForageProvider){
+//   $localForageProvider.setNotify(true, true); // itemSet, itemRemove
+// }]);
+// // =======
 
-// .config(function($routeProvider,$httpProvider){
-//     $routeProvider
-//     .when('/', {
-//         templateUrl: 'index.html',
-//         controller : 'PeerController'
-//     })
-// })
-// Main app stuff here
->>>>>>> rebase commit
+// // .config(function($routeProvider,$httpProvider){
+// //     $routeProvider
+// //     .when('/', {
+// //         templateUrl: 'index.html',
+// //         controller : 'PeerController'
+// //     })
+// // })
+// // Main app stuff here
+// >>>>>>> rebase commit
