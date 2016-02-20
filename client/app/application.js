16d99d97a8c7be51392e9351ce3841b5165d648c
Joseph Ashby <jashby473@gmail.com>
2016-02-19 19:57:09 -0800

    exprimenting with ui router

--- client/app/application.js
+++ client/app/application.js
@@ -7,19 +7,45 @@ var forinlanguagesApp = angular.module('forinlanguages', [
   'ui.router'
 ]);
 
-forinlanguagesApp.config(function($routeProvider){
-    $routeProvider
-    .when('/', {
-        templateUrl: 'views/landingPage.html',
-        controller : 'PeerController'
-    })
-    .when('/app', {
-        templateUrl:'views/app.html',
-        controller : 'PeerController'
-    });
-});
+// forinlanguagesApp.config(function($routeProvider){
+//     $routeProvider
+//     .when('/', {
+//         templateUrl: 'views/landingPage.html',
+//         controller : 'PeerController'
+//     })
+//     .when('/app', {
+//         templateUrl:'views/app.html',
+//         controller : 'PeerController'
+//     });
+// });
 
-// forinlanguagesApp.config(function($stateProvider, $urlRouterProvider){
-//   $urlRouterProvider.otherwise("")
-// })
+forinlanguagesApp.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise("/app");
+
+  $stateProvider
+  .state("landing" , {
+    url: "/",
+    templateUrl: "views/landingPage.html" ,
+    controller: function($scope){
+        $scope.x = 0;
+        $scope.user = "joe";
+    }
+  })
+  .state("landing.test",{
+    url:"/landing/test",
+    templateUrl: "views/test.html",
+    controller: function($scope){
+        $scope.things = [1,23,44335,6364,'done'];
+    }
+  })
+  .state("app" , {
+    url: "/app",
+    templateUrl: "views/app.html",
+    controller: "PeerController"
+  })
+  .state("landing.test.user",{
+    url: "/lsnding/test/user",
+    templateUrl:"views/user.test.html",
+  });
+})
 
