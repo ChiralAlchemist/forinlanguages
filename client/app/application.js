88b3829a4fecd91e3f653e66ef8f46e94d72a663
Joseph Ashby <jashby473@gmail.com>
2016-02-21 22:34:26 -0800

    added ui router

--- client/app/application.js
+++ client/app/application.js
@@ -2,10 +2,23 @@ angular.module('forinlanguages', [
   'file-model',
   'LocalForageModule',
   'forinlanguages.services',
-  'forinlanguages.peer'
+  'forinlanguages.peer',
+  'ui.router'
 ])
 
+.config(function($stateProvider, $urlRouterProvider){
 
+    $urlRouterProvider.otherwise("/app");
+
+    $stateProvider
+    .state('app', {
+        url: "/app",
+        views: {
+            "ConnectionView": {templateUrl:"../views/connection.html"},
+            "ChatView" : {templateUrl:"../views/chat.html"}, 
+        }
+    })
+})
 
 // .config(function($routeProvider,$httpProvider){
 //     $routeProvider
